6561753ded6c68dfff8a2214b58977d94ccf9c07
Dino Hurem <dino.hurem@authoritypartners.com>
2019-07-12 15:30:52 +0200

    Adding navbar and initial UI changes

--- ui/desktop/src/Quattro.Desktop/MainWindow.xaml.cs
+++ ui/desktop/src/Quattro.Desktop/MainWindow.xaml.cs
@@ -24,5 +24,22 @@ namespace Quattro.Desktop
         {
             InitializeComponent();
         }
+
+        private void ButtonPopupLogout_OnClick(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void ButtonOpenMenu_OnClick(object sender, RoutedEventArgs e)
+        {
+            ButtonOpenMenu.Visibility = Visibility.Collapsed;
+            ButtonCloseMenu.Visibility = Visibility.Visible;
+        }
+
+        private void ButtonCloseMenu_OnClick(object sender, RoutedEventArgs e)
+        {
+            ButtonOpenMenu.Visibility = Visibility.Visible;
+            ButtonCloseMenu.Visibility = Visibility.Collapsed;
+        }
     }
 }
